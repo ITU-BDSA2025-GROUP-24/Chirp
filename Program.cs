11b760a262a6c6e1959489ea04aba5208c9d1685
Aron Hansen <arha@itu.dk>
2025-08-29 11:17:49 +0200

    Comma support in messages

--- Program.cs
+++ Program.cs
@@ -10,10 +10,16 @@ catch (Exception e)
 
 foreach (var file in files)
 {
-    string[] parts = file.Split(',');
+    string[] parts = file.Split('"');
+    if (parts[0].Contains(','))
+    {
+        parts[0] = parts[0].Split(',')[0];
+    }
+    parts[2] = parts[2].Split(',')[1];
     long tid = long.Parse(parts[2]);
     DateTime TidDato = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
     TidDato = TidDato.AddSeconds(tid);
     TidDato = TidDato.AddMinutes(120);
+    
     Console.WriteLine(parts[0] + " @ " + TidDato.ToShortDateString() + " "+ TidDato.ToLongTimeString() + " " + parts[1]);
 }
