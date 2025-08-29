0e5c402007f6105f48f6dba06fa846a5bea7ba52
chris <chria@itu.dk>
2025-08-29 10:48:25 +0200

    Almost Done week 1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
-﻿string[] files = File.ReadAllLines(@"chirp_cli_db.csv");
+﻿string[] files = new string[10];
+try
+{
+    files = File.ReadAllLines(@"chirp_cli_db.csv");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
 
 foreach (var file in files)
 {
-    Console.WriteLine(file);    
+    string[] parts = file.Split(',');
+    long tid = long.Parse(parts[2]);
+    DateTime TidDato = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    TidDato = TidDato.AddSeconds(tid);
+    TidDato = TidDato.AddMinutes(120);
+    Console.WriteLine(parts[0] + " @ " + TidDato.ToShortDateString() + " "+ TidDato.ToLongTimeString() + " " + parts[1]);
 }
