f7e3bcf6f85c85d9ccd0e52cc28e1212846a3d23
Line Præstegaard <ljni@itu.dk>
2025-09-05 11:34:56 +0200

    Started working on CSVDatabase

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -1,7 +1,28 @@
 
+using System.Globalization; 
+using System.Reflection; 
+using System.Reflection.Metadata;
+using CsvHelper; 
+
+
 namespace SimpleDB;
 
-public class CSVDatabase
+public class CSVDatabase<T> : IDatabaseRepository<T>
 {
-    
+	string filePath; 
+	
+	public CSVDatabase(string filePath = "../../chirp_cli_db.csv")
+	{
+		this.filePath = filePath; 
+		
+		foreach(Property in prop in typeof(T).GetProperties())
+		{
+			
+		}	
+    }
+	
+	public IEnumerable<T> Read(int? limit = null)
+	{
+	  
+	}
 }
