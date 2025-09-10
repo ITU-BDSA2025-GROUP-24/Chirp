4fe33d191498766eda156c7f311950c67a6f9153
Line Præstegaard <ljni@itu.dk>
2025-09-10 12:31:50 +0200

    Changed directory path for .csv file
    Changed directory path for the .csv file and added a check that prints where the application looks for the file.

--- SimpleDB/CsvDatabase.cs
+++ SimpleDB/CsvDatabase.cs
@@ -9,9 +9,17 @@ public sealed class CsvDatabase<T> : IDatabaseRepository<T>
 {
 	private string filePath;
 
-	public CsvDatabase(string filePath = "../../chirp_cli_db.csv")
+	public CsvDatabase(string filePath = "../Chirp/data/chirp_cli_db.csv")
 	{
 		this.filePath = filePath;
+		
+		/*   //Checking path for .csv
+		 Console.WriteLine($"Current working directory: {Directory.GetCurrentDirectory()}");
+		Console.WriteLine($"Looking for file at relative path: {filePath}");
+		Console.WriteLine($"Full resolved path: {Path.GetFullPath(filePath)}");
+		Console.WriteLine($"File exists at that path: {File.Exists(filePath)}");
+		Console.WriteLine();
+		*/
 	}
 
 	public IEnumerable<T> Read(int? limit = null)
