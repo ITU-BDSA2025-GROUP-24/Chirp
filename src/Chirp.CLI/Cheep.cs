e5193a06a826ae2e971e3a77dc7a657366e76f4c
chris <chria@itu.dk>
2025-09-12 11:36:47 +0200

    small changes
    dont mind me

--- src/Chirp.CLI/Cheep.cs
+++ src/Chirp.CLI/Cheep.cs
@@ -1,13 +1,3 @@
 ﻿namespace Chirp.CLI;
 
 public record Cheep(string Author, string Message, long Timestamp);
-/*public record Cheep
-{
-    public string Author
-    {  
-        get;
-        set;
-    }
-    public string Message { get; set; }
-    public long Timestamp { get; set; }
-}*/
