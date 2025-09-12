70f0c69bba0053dc2be4b2175bbec24fc387e324
chris <chria@itu.dk>
2025-09-12 10:21:12 +0200

    Fixed a path bug and added 1 test
    What he said

--- test/UnitTests/UnitTest1.cs
+++ test/UnitTests/UnitTest1.cs
@@ -2,9 +2,17 @@
 
 public class UnitTest1
 {
+    public record Cheep(string Author, string Message, long Timestamp);
     [Fact]
     public void Test1()
     {
-
+        var Author = "Chris";
+        var Message = "Hey There";
+        var TimeStamp = 2242298352;
+        
+        var MyCheep = new Cheep(Author, Message, TimeStamp);
+        Assert.Equal(Author, MyCheep.Author);
+        Assert.Equal(Message, MyCheep.Message);
+        Assert.Equal(TimeStamp, MyCheep.Timestamp);
     }
 }
