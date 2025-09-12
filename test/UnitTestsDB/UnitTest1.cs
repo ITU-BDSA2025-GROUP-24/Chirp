4648e17bd2514d0c754818ec6c3655a26ebea6fe
Phongchai <phoc@itu.dk>
2025-09-12 13:07:18 +0200

    Database storage testing

--- test/UnitTestsDB/UnitTest1.cs
+++ test/UnitTestsDB/UnitTest1.cs
@@ -1,10 +1,32 @@
 ﻿namespace UnitTestsDB;
 
+using Xunit;
+using Chirp.CLI;
+
 public class UnitTest1
 {
+    
+    
     [Fact]
-    public void Test1()
+    public void StoringCheepToDatabase()
     {
+        
+        //arrange
+        var author = "User";
+        var message = "Storage";
+        var timestamp = 1234567890L;
+        var cheep = new Cheep(author, message, timestamp);
+        
+        Cheep[] storage = { cheep };
+        
+        //act
+        var savedCheep = storage[0];
 
+         
+            
+        //assert
+        Assert.Equal(author, savedCheep.Author);
+        Assert.Equal(message, savedCheep.Message);
+        Assert.Equal(timestamp, savedCheep.Timestamp);
     }
 }
