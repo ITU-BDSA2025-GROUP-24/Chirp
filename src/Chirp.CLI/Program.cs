740f400f3f43a35c306596a618cf0a2314b2f0f9
Line Præstegaard <ljni@itu.dk>
2025-09-16 12:59:32 +0200

    Removed unnecessary code
    Removed the Itemgroup that added the .csv to Chirp.CLI.csproj when creating a .exe file.
    Renamed commands from 'post' to 'cheep' and removed 'version' information.
    Removed manual args checking, Docopt takes care of this function.
    Moved print statements to UserInterface.cs.
    Removed code that had been commented out.

--- src/Chirp.CLI/Program.cs
+++ src/Chirp.CLI/Program.cs
@@ -1,41 +1,33 @@
 ﻿
-using System.Collections;
 using Chirp.CLI;
 using SimpleDB;
 using DocoptNet;
+
 IDatabaseRepository<Cheep> dbRepository = new CsvDatabase<Cheep>();
+
 const string Usage = @"Chirp.
 
 Usage:
   chirp read
-  chirp post <message>...
+  chirp cheep <message>...
   chirp (-h | --help)
-  chirp --version
-
-Options:
-  -h --help     Show help.
-  --version     Show version.
 ";
 
 try
 {
-    var argsDict = new Docopt().Apply(Usage, args, version: "Chirp 1.0", exit: true);
+    var argsDict = new Docopt().Apply(Usage, args, exit: true);
 
     if (argsDict["read"].IsTrue)
     {
-        // TODO: load and print cheeps from your DB
-        Console.WriteLine("Reading cheeps...");
-        // e.g., var cheeps = Parser.ComposeCheep(); foreach (var c in cheeps) UserInterface.WriteOutCheep(c);
+        //Reads and prints cheeps from database
+        read(); 
+        
     }
-    else if (argsDict["post"].IsTrue)
+    else if (argsDict["cheep"].IsTrue)
     {
-        var parts = ((IEnumerable)argsDict["<message>"].AsList)
-            .Cast<object>()
-            .Select(o => o?.ToString() ?? "");
-        var message = string.Join(" ", parts);
-        Console.WriteLine($"Posting: {message}");
+		//Adds cheep to database
+        var message = string.Join(" ", argsDict["<message>"].AsList.Cast<string>());
         cheep(message);
-        // TODO: save the cheep, e.g., Parser.StoreCheep(message);
     }
 }
 catch (DocoptInputErrorException e)
@@ -45,90 +37,34 @@ catch (DocoptInputErrorException e)
     Environment.Exit(1);
 }
 
-//dbRepository - 'Link'/representation of database 
-if (args.Length == 0)
-{
-    Console.WriteLine("Please 'read' or 'cheep'");
-    return;
-}
-
-if (args[0] == "read")
-{
-    read();
-}
-else if (args[0] == "cheep")
-{
-    cheep(args[1]);
-}
-
 void read()
 {
+	UserInterface.ReadingCheeps();
     var cheeps = dbRepository.Read();
     
     //Are there any recorded cheeps?
     if (!cheeps.Any())
     {
-        Console.WriteLine("No cheeps yet");
+        UserInterface.NoCheeps();
         return;
     }
 
-    foreach (var c in cheeps)
-    {
-        UserInterface.WriteOutCheep(c);
+   	foreach (var cheep in cheeps)
+	{
+        UserInterface.WriteOutCheep(cheep);
     }
 }
 
-void cheep(string cheeping)
+void cheep(string message)
 {
-    string name = Environment.UserName;
-    long time = DateTimeOffset.Now.ToUnixTimeSeconds();
+	UserInterface.PostingCheep(message); 
     
     //Create and store new cheep using SimpeDB
-    var newCheep = new Cheep(name, cheeping, time);
+    var newCheep = new Cheep(
+		Environment.UserName, 
+		message, 
+		DateTimeOffset.Now.ToUnixTimeSeconds()
+	);
+
     dbRepository.Store(newCheep);
 }
-
-/*
-string[] files = new string[10];
-try
-{
-    files = File.ReadAllLines(@"chirp_cli_db.csv");
-}
-catch (Exception e)
-{
-    Console.WriteLine(e.Message);
-}
-
-if (args[0] == "read")
-{
-    read();
-} else if (args[0] == "cheep")
-{
-    cheep(args[1]);
-    Cheep hey = new Cheep();
-}
-void read()
-{ 
-    var cheeps = Parser.ComposeCheep(args);
-    foreach (var c in cheeps)
-    {
-       UserInterface.WriteOutCheep(c);
-    }
-}
-
-void cheep(string cheeping)
-{
-    StringBuilder s = new StringBuilder();
-    string name = Environment.UserName;
-    String tidDato = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToLongTimeString() + " ";
-    s.Append(name + " @ " + tidDato + cheeping);
-    String newCheep =  s.ToString();
-    Console.WriteLine(newCheep);
-    StreamWriter sw = new StreamWriter(@"chirp_cli_db.csv",true);
-    s.Clear();
-
-    long time = DateTimeOffset.Now.ToUnixTimeSeconds();
-    s.Append(name + "," + '"' + cheeping + '"' + "," +time);
-    sw.WriteLine(s.ToString());
-    sw.Close();
-}*/
--- src/Chirp.CLI/UserInterface.cs
+++ src/Chirp.CLI/UserInterface.cs
@@ -9,4 +9,28 @@ public class UserInterface
             .ToString("yyyy-MM-dd HH:mm");
         Console.WriteLine($"[{when}] {cheep.Author}: {cheep.Message}");
     }
+
+	//Instructions for user
+	public static void Instructions()
+	{
+		Console.WriteLine("Please 'read' or 'cheep'");
+	}
+
+	//Message telling user that cheeps are being read
+	public static void ReadingCheeps()
+	{
+		Console.WriteLine("Reading cheeps...");
+	}
+
+	//Message telling user that their cheep is being cheeped 
+	public static void PostingCheep(string message) 
+	{
+		Console.WriteLine($"Cheeping: {message}");
+	}
+
+	//If there are no cheeps
+	public static void NoCheeps()
+	{
+		Console.WriteLine("No cheeps yet");
+	}
 }
