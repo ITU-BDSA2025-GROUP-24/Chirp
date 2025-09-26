300a76126c5c2ba5c2159059dd634d5d827facac
Radmehr <rads@itu.dk>
2025-09-26 11:44:47 +0200

    Created a class for end to end tests

---
+++ test/Endtoendtests/endtoendtests.cs
@@ -0,0 +1,11 @@
+namespace Endtoendtests;
+
+using Xunit;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Linq;
+using SimpleDB;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
