5542e4735a25b014c9e2e913806e3521ce2247ab
Aron Hansen <arha@itu.dk>
2025-10-10 11:59:08 +0200

    Addition of migrations particularily to Cheep.cs "LimitAuthorInfoStringLength.cs"

--- src/CheepDTO.cs
+++ src/CheepDTO.cs
@@ -1,10 +1,15 @@
-﻿namespace MyChat.Razor;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyChat.Razor;
 
 public class CheepDTO
 {
     public int CheepId { get; set; }
+    [Required]
     public required string Author { get; set; }
+    [Required]
     //public required string AuthorDTO Author { get; set; }
     public required string Cheep  { get; set; }
+    [Required]
     public required DateTime Timestamp { get; set; }
 }
