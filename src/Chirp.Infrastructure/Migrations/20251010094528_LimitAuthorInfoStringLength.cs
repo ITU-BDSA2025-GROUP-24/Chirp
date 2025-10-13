628a2d795f32d8563f38d4179840bf5cfd06b3af
Line Præstegaard <ljni@itu.dk>
2025-10-13 14:55:19 +0200

    Added using directives
    Added using directives

--- src/Chirp.Infrastructure/Migrations/20251010094528_LimitAuthorInfoStringLength.cs
+++ src/Chirp.Infrastructure/Migrations/20251010094528_LimitAuthorInfoStringLength.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Chirp.Infrastructure; 
 
 #nullable disable
 
--- src/Chirp.Infrastructure/Migrations/20251010095556_NonNullDTO.cs
+++ src/Chirp.Infrastructure/Migrations/20251010095556_NonNullDTO.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Chirp.core;
 
 #nullable disable
 
