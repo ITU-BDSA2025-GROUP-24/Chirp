e0106c11be346cb3f4c423976f5ac8a0987e74a3
Line Præstegaard <ljni@itu.dk>
2025-10-13 14:45:19 +0200

    Revert "Update ChirpDBContext.cs"
    This reverts commit ae0f890820799e62e7c5f44779a5612a2d89ba06.

--- src/Chirp.Infrastructure/ChirpDBContext.cs
+++ src/Chirp.Infrastructure/ChirpDBContext.cs
@@ -1,23 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
 namespace Chirp.Infrastructure
+
+public class ChirpDBContext : DbContext
 {
+    public DbSet<Cheep> Cheeps { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public string DbPath { get; }
 
-    public class ChirpDBContext : DbContext
+    public ChirpDBContext(DbContextOptions<ChirpDBContext> options) : base(options)
     {
-        public DbSet<Cheep> Cheeps { get; set; }
-        public DbSet<Author> Authors { get; set; }
-        public string DbPath { get; }
-
-        public ChirpDBContext(DbContextOptions<ChirpDBContext> options) : base(options)
-        {
-            var path = Path.GetTempPath();
-            DbPath = Path.Join(path, "chirp.db");
-            Console.WriteLine(DbPath);
-        }
-
-        // Configures EF to create a Sqlite database file in the special "local" folder
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite($"Data Source={DbPath}");
+        var path = Path.GetTempPath();
+        DbPath = Path.Join(path, "chirp.db");
+        Console.WriteLine(DbPath);
     }
+
+    // Configures EF to create a Sqlite database file in the special "local" folder
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
 }
