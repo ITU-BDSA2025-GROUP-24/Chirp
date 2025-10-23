aa91d00d3117e87e24b80aa011188533f64c139a
Line Præstegaard <ljni@itu.dk>
2025-10-23 12:35:43 +0200

    Started implementing an AddCheep function

---
+++ src/Chirp.Web/Pages/AddCheep.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Chirp.Infrastructure;
+using Chirp.Core; 
+
+
+namespace Chirp.Web.Pages
+{
+    public class AddCheepModel : PageModel
+    {
+        private readonly ICheepRepository _repository;
+
+        public AddCheepModel(ICheepRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task OnPostAsync(String Author, String Cheep)
+        {
+            var cheepDto = new CheepDTO 
+            { 
+                Author = Author, 
+                Cheep = Cheep,  // or Message, depending on your DTO property name
+                Timestamp = DateTime.Now
+            };
+    
+            await _repository.CreateCheep(cheepDto);
+        }
+    }
+
+}
