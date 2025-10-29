54fb52288a8693b34c5f30ddecd698a1d00370f2
Phongchai <phoc@itu.dk>
2025-10-29 13:58:27 +0100

    Cheep UnitTest created

--- Test/UnitTest/Chirp.Tests/CheepTests.cs
+++ Test/UnitTest/Chirp.Tests/CheepTests.cs
@@ -1,10 +1,30 @@
+using Chirp.Infrastructure;
+
 namespace Chirp.Tests;
 
 public class CheepTests
 {
+    public record Cheep(int CheepId, int AuthorId, Author Author, string Text, DateTime TimeStamp);
     [Fact]
     public void Test1()
     {
-
+        //Arrange
+        var CheepId = 1234;
+        var AuthorId = 9876;
+        var author = new Author { AuthorId = AuthorId, Name = "Creator" };
+        var Text = "Let there be text!";
+        var TimeStamp = DateTime.Now;
+        
+        
+        //Act
+        var MyCheep = new Cheep(CheepId, AuthorId, author, Text, TimeStamp);
+        
+        
+        //Assert
+        Assert.Equal(CheepId, MyCheep.CheepId);
+        Assert.Equal(AuthorId, MyCheep.AuthorId);
+        Assert.Equal(author.Name, MyCheep.Author.Name);
+        Assert.Equal(Text, MyCheep.Text);
+        Assert.Equal(TimeStamp, MyCheep.TimeStamp, TimeSpan.FromSeconds(1));
     }
 }
