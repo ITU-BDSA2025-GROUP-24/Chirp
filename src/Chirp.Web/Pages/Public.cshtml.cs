1790eac15b37c33a2a9a037c48924e23d187e769
chris <chria@itu.dk>
2025-11-12 14:27:12 +0100

    More pages
    buttons for more pages

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -7,6 +7,14 @@ namespace Chirp.Web.Pages;
 public class PublicModel : PageModel
 {
     private readonly ICheepRepository _repository;
+    
+    public int CurrentPage { get; private set; } = 1;
+    
+    [BindProperty(SupportsGet = true)]
+    public int Page { get; set; } = 1;
+    
+    public int TotalPages { get; set; }
+    
     public required IEnumerable<CheepDTO> Cheeps { get; set; }
     public AddCheepModel AddCheepModel{ get; set; }
 
@@ -16,11 +24,12 @@ public class PublicModel : PageModel
         AddCheepModel = new AddCheepModel(repository);
     }
 
- public async Task<ActionResult> OnGet()
+    public async Task<IActionResult> OnGetAsync([FromQuery(Name = "page")] int page = 1)
     {
-        Cheeps = await _repository.ReadCheep();
+        CurrentPage = page < 1 ? 1 : page;
+        Cheeps = await _repository.ReadCheep(CurrentPage);
+        Console.WriteLine($"Page={CurrentPage}");
         return Page();
-        
     }
 
     [BindProperty]
