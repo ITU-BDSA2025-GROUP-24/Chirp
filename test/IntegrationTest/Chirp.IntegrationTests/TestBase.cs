2adf2e5065cfb9ab7dacf2c80a440282eb9ba6af
Phongchai <phoc@itu.dk>
2025-11-28 11:21:02 +0100

    AuthorRepositoryIntegrationTest

---
+++ test/IntegrationTest/Chirp.IntegrationTests/TestBase.cs
@@ -0,0 +1,18 @@
+﻿using Chirp.Infrastructure;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chirp.IntegrationTests;
+
+public abstract class TestBase
+{
+    protected DbContextOptions<ChirpDBContext> CreateInMemoryOptions()
+    {
+        var conn = new SqliteConnection("Filename=:memory:");
+        conn.Open();
+
+        return new DbContextOptionsBuilder<ChirpDBContext>()
+            .UseSqlite(conn)
+            .Options;
+    }
+}
