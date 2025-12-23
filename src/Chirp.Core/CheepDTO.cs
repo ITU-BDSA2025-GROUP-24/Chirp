84b7d8922ee3a25e62026ebae773f72e177c5432
Line Præstegaard <ljni@itu.dk>
2025-12-23 10:13:30 +0100

    Add comments to DTOs
    AuthorDTO and CheepDTO now contain comments that explain our code

--- src/Chirp.Core/CheepDTO.cs
+++ src/Chirp.Core/CheepDTO.cs
@@ -4,8 +4,18 @@ namespace Chirp.Core;
 
 public class CheepDTO
 {
+    /*
+    CheepId - Guid used to make ID's unpredictable, 
+    since they are not created by incrementing the previous ID
+    */
     public Guid CheepId { get; set; }
+   
+    //Cheep author - AuthorDTO type. Required! 
     public required AuthorDTO Author { get; set; }
+    
+    //Cheep text, also required - You cannot make a cheep without the cheep
     public required string Cheep  { get; set; }
+    
+    //When the cheep was created - Required, used to sort cheeps chronologically 
     public required DateTime TimeStamp { get; set; }
 }
